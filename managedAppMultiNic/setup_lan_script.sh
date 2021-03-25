d4d0dab21728fd1023c88dee4745048c10a2082f
dariuszSki <dariusz.sliwinski@netfoundry.io>
2021-03-25 13:27:25 -0400

    consolidated managed app folers into only ubuntu

--- managedAppMultiNic/setup_lan_script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-IPADDRESS=`eval cat /var/lib/cloud/network.cfg | jq -r '.vnfMetadata.interface[] | select(.vmSwitchType == "LAN") | .ipConfigurations[].ipAddress'`
-NETWORK=`eval cat /var/lib/cloud/network.cfg | jq -r --arg IPADDRESS $IPADDRESS '.network.interface[].ipv4 | select(.ipAddress[].privateIpAddress == $IPADDRESS) | .subnet[].address'`
-PREFIX=`eval cat /var/lib/cloud/network.cfg | jq -r --arg IPADDRESS $IPADDRESS '.network.interface[].ipv4 | select(.ipAddress[].privateIpAddress == $IPADDRESS) | .subnet[].prefix'`
-/bin/nmcli connection modify 'System eth1' ipv4.method manual ipv4.addr $IPADDRESS/$PREFIX connection.zone trusted
